69be21504c952bded1bcf45d33b1e886d5ec56b2
Tanush <tchilakwad@gmail.com>
2026-07-06 06:06:22 +0000

    requisities

---
+++ build/CMakeFiles/OpenGLProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLProject.dir/src/glad.c.o"
+  "CMakeFiles/OpenGLProject.dir/src/glad.c.o.d"
+  "OpenGLProject"
+  "OpenGLProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OpenGLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
